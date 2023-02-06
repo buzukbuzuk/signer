4937f17a057b9a310d182d97c685d3d38481d4de
buzukbuzuk <75903448+buzukbuzuk@users.noreply.github.com>
2023-02-07 00:26:42 +0600

    Add files via upload

---
+++ signer/main.go
@@ -0,0 +1,25 @@
+package signer
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	tempChan := make(chan int)
+
+	wg := &sync.WaitGroup{}
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(in chan int, i int) {
+			fmt.Println("Отравботал джоб", i)
+			time.Sleep(2 * time.Second)
+			wg.Done()
+		}(tempChan, i)
+	}
+
+	wg.Wait()
+
+}
